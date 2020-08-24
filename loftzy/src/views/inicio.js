a230c4a6ea8b00e8d9ccd060ad09ecfb69956036
David Patiño <davidalexander1996@gmail.com>
2020-08-24 15:41:44 -0500

    actualización de inicio

--- loftzy/src/views/inicio.js
+++ loftzy/src/views/inicio.js
@@ -1,31 +1,27 @@
 import React , {Component}from 'react';
 
 import InputWithLabel from '../componentes/InputWithLabel';
+import ButtonAdmin from '../componentes/ButtonMenu';
 
 
-class AgregarResidente extends Component {
+class inicio extends Component {
 
     render(){
         return(
-            <table>
-                <tr>
-                    <th>
-                        <button>Atras</button>
-                     
-                    </th>
-                    <th>
-                       <InputWithLabel label="nombre"></InputWithLabel>
-                       <InputWithLabel label="telefono"></InputWithLabel>
-                       <InputWithLabel label="correo"></InputWithLabel>
-                       <InputWithLabel label="usuario"></InputWithLabel>
-                       <InputWithLabel label="contraseña"></InputWithLabel>
-                       <button>Agregar</button>
-                    </th>
-                </tr>
-            </table>
+            <div>
+                <InputWithLabel label="nombre"></InputWithLabel>
+                <InputWithLabel label="Contraseña"></InputWithLabel>
+                
+            </div>
+            <div>
+                <ButtonAdmin label="Acceder" />
+            </div>
+
+                       
+                      
         )
     }
 }
 
 
-export default AgregarResidente;
+export default inicio;
