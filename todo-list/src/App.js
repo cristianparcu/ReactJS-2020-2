155755ef9d609b0339f31ee8d1ea2f5d747f5caa
Luis Martinez <luisemartinezcruz@gmail.com>
2020-09-07 11:42:12 -0500

    Add components, logic and styles to them
    - Add New Todo, which is a form that allows to add a new Todo
    - Add Todo which is the todo with all the details (the description, id
    and the completed state)
    - Add TodoList which is a container for the individual Todos

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,11 +1,38 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+import NewTodo from "./components/newTodo/NewTodo";
+import TodoList from "./components/todoList/TodoList";
+import styles from "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  function addTodo(todo) {
+    setTodos([todo, ...todos]);
+  }
+
+  function changeComplete(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
   return (
     <div>
-      <p>To Do List</p>
+      <h1 className={styles.header}>To Do List</h1>
+      <NewTodo addTodo={addTodo} />
+      <TodoList todos={todos} changeComplete={changeComplete} removeTodo={removeTodo} />
     </div>
   );
 }
