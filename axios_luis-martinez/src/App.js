7b39344b6857bd9a7926b845aa1fc646a0fa11fa
Luis Martinez <luisemartinezcruz@gmail.com>
2020-09-20 23:29:16 -0500

    Add Axios homework
    Made JSON file with 10 articles with mock data
    Axios get request
    News Component

---
+++ axios_luis-martinez/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import News from "./components/News";
+
+export default class App extends Component {
+    constructor() {
+        super();
+        this.state = { news: [] };
+    }
+
+    componentDidMount() {
+        axios.get("./news.json").then((res) => {
+            console.log(res.data);
+            const currentNews = res.data;
+            this.setState({ news: currentNews });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.news.map((i) => {
+                    return (
+                        <News
+                            key={i.id}
+                            author={i.author}
+                            body={i.body}
+                            title={i.title}
+                        />
+                    );
+                })}
+            </div>
+        );
+    }
+}
---
+++ axios_luis-martinez/src/components/News.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./News.css";
+
+const News = (props) => {
+    return (
+        <div className={styles.card}>
+            <h3 className={styles.title}>{props.title}</h3>
+            <p className={styles.body}>{props.body}</p>
+            <h3 className={styles.author}>{props.author}</h3>
+        </div>
+    );
+};
+
+export default News;
