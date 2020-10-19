eccf66c098d676fbdf48bffe5ffa72f608c12417
Cristian Paredes <cparedes@hugeinc.com>
2020-10-19 15:15:18 -0500

    Redux advanced: cleaning our reducers with utility function

--- redux-advanced/src/store/reducers/counter.js
+++ redux-advanced/src/store/reducers/counter.js
@@ -1,29 +1,39 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     counter: 0
 }
 
+const increment = (state, payload) => {
+    const counter = state.counter + 1;
+    return updateObject(state, {counter: counter});
+}
+
+const decrement = (state, payload) => {
+    const counter = state.counter - 1;
+    return updateObject(state, {counter: counter});
+}
+
+const add = (state, payload) => {
+    const counter = state.counter + payload.value;
+    return updateObject(state, {counter: counter});
+}
+
+const subtract = (state, payload) => {
+    const counter = state.counter - payload.value;
+    return updateObject(state, {counter: counter});
+}
+
 const reducer = (state = initialState, action) => {
+    const payload = action.payload;
+
     switch (action.type) {
-        case actionTypes.INCREMENT:
-            return {
-                counter: state.counter + 1
-            }
-        case actionTypes.DECREMENT:
-            return {
-                counter: state.counter - 1
-            }
-        case actionTypes.ADD:
-            return {
-                counter: state.counter + action.payload.value
-            }
-        case actionTypes.SUBTRACT:
-            return {
-                counter: state.counter - action.payload.value
-            }
-        default:
-            return state;
+        case actionTypes.INCREMENT: return increment(state, payload);
+        case actionTypes.DECREMENT: return decrement(state, payload);
+        case actionTypes.ADD:       return add(state, payload);
+        case actionTypes.SUBTRACT:  return subtract(state, payload);
+        default:                    return state;
     }
 }
 
