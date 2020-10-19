4fa91b3c0d8279db83f3c9a0d51765e40d1a4113
SocksHead <juancapoar@gmail.com>
2020-10-18 22:54:54 -0500

    taller-redux

--- redux-basics/src/store/actions.js
+++ redux-basics/src/store/actions.js
@@ -1,4 +1,5 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-export const ADD = 'ADD';
-export const SUBTRACT = 'SUBTRACT';
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+export const ADD = "ADD";
+export const SUBTRACT = "SUBTRACT";
+export const SAVE = "SAVE";
